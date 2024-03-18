c9864cf06441de874e90d95d46d7a9bd88c3add0
yuvarajukallepati <140689178+yuvarajukallepati@users.noreply.github.com>
2024-03-18 20:27:09 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,28 @@
+import random
+length=int(input("enter the length of password: "))
+if(length>3):
+    n_letters=int(input("enter the no of letters: "))
+    n_numbers=(int(input("enter the no of numbers:")))
+
+    letters=['A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    numbers=['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    pwd=[]
+    for i in range(n_letters):
+        char=random.choice(letters)
+        pwd += char
+    for i in range(n_numbers):
+        num=random.choice(numbers)
+        pwd += num
+    print(pwd)
+    random.shuffle(pwd)
+    print(pwd)
+
+    password=""
+    for char in pwd:
+        password+=char
+    print(password)
+
+else:
+    print("password length must be greater than 3 digits")
+    
